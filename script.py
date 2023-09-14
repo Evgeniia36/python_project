0d6837e0aabaeb25e6e72aef4fb61131878e138e
Evgeniia Malygina <135066209+Evgeniia36@users.noreply.github.com>
2023-09-14 11:41:47 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
+money = int(input("Введите сумму целым числом:"))
+
+deposit = list(map(lambda x: round(x*money/100), per_cent.values()))
+
+#print (deposit)
+
+max_value = max(deposit)
+
+print ("Максимальная сумма, которую вы можете заработать — ", max_value)
